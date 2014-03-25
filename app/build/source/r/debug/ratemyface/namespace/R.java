74afee4e448dab0b580106f108be20b0b211e1e3
Teja Ravipati <ravipati@umich.edu>
2014-03-25 15:29:56 -0400

    simplify camera implementation and initial facial detection

--- app/build/source/r/debug/ratemyface/namespace/R.java
+++ app/build/source/r/debug/ratemyface/namespace/R.java
@@ -16,11 +16,15 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int n10680732=0x7f020001;
+        public static final int team_photo=0x7f020002;
+        public static final int travipati=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
+        public static final int action_settings=0x7f080003;
         public static final int btToggle=0x7f080000;
-        public static final int ivFace=0x7f080001;
+        public static final int ivFace=0x7f080002;
+        public static final int ratingBar=0x7f080001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- app/src/main/java/ratemyface/namespace/MainActivity.java
+++ app/src/main/java/ratemyface/namespace/MainActivity.java
@@ -1,27 +1,23 @@
 package ratemyface.namespace;
 
 import java.io.ByteArrayInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.PointF;
 import android.hardware.Camera;
-import android.hardware.Camera.AutoFocusCallback;
-import android.hardware.Camera.Face;
-import android.hardware.Camera.FaceDetectionListener;
 import android.hardware.Camera.PictureCallback;
-import android.hardware.Camera.ShutterCallback;
-import android.net.Uri;
+import android.media.FaceDetector;
 import android.os.Bundle;
-import android.provider.MediaStore.Audio.Media;
 import android.app.Activity;
-import android.content.ContentValues;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -42,19 +38,38 @@ public class MainActivity extends Activity implements OnClickListener, Runnable 
 		setContentView(R.layout.main);
 		initialize();
 	}
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (camera != null){
+            camera.release();
+            camera = null;
+        }
+    }
 	
 	private void initialize() {
-        rb = (RatingBar) findViewById(R.id.ratingBar);
-        iv = (ImageView) findViewById(R.id.ivFace);
 		b = (Button) findViewById(R.id.btToggle);
 		b.setOnClickListener(this);
-		
-		camera = Camera.open();
-//		camera.setFaceDetectionListener(faceDetectionListener);
-		
+        iv = (ImageView) findViewById(R.id.ivFace);
+        rb = (RatingBar) findViewById(R.id.ratingBar);
+
+        camera = getCameraInstance();
+
 		ScheduledExecutorService scheduleTaskExecutor = Executors.newScheduledThreadPool(5);
 		scheduleTaskExecutor.scheduleAtFixedRate(this, 0, 2, TimeUnit.SECONDS);
 	}
+
+    public static Camera getCameraInstance() {
+        Camera c = null;
+        try {
+            c = Camera.open();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return c;
+    }
 
 	@Override
 	public void onClick(View v) {
@@ -74,60 +89,39 @@ public class MainActivity extends Activity implements OnClickListener, Runnable 
 	@Override
 	public void run() {
 		if (isCapturing) {
-			camera.takePicture(myShutterCallback, myPictureCallback_RAW, myPictureCallback_JPG);
+			camera.takePicture(null, null, myPictureCallback);
 		}
 	}
 
-	FaceDetectionListener faceDetectionListener = new FaceDetectionListener() {
-		@Override
-		public void onFaceDetection(Face[] faces, Camera camera) {
-			if (faces.length == 1) {
-                rb.setNumStars(0);
-			}
-		}
-	};
-	
-	AutoFocusCallback myAutoFocusCallback = new AutoFocusCallback() {
-		@Override
-		public void onAutoFocus(boolean arg0, Camera arg1) {
-			b.setEnabled(true);
-		}
-	};
-	
-	ShutterCallback myShutterCallback = new ShutterCallback() {	
-		@Override
-		public void onShutter() {}
-	};
-	
-	PictureCallback myPictureCallback_RAW = new PictureCallback() {
-		@Override
-		public void onPictureTaken(byte[] arg0, Camera arg1) {}
-	};
-	
-	PictureCallback myPictureCallback_JPG = new PictureCallback() {
+	PictureCallback myPictureCallback = new PictureCallback() {
 		@Override
 		public void onPictureTaken(byte[] arg0, Camera arg1) {
-			
-			InputStream is = new ByteArrayInputStream(arg0);
-			Bitmap bmp = BitmapFactory.decodeStream(is);
-			iv.setImageBitmap(bmp);
-			
-/*			Uri uriTarget = getContentResolver().insert(Media.EXTERNAL_CONTENT_URI, new ContentValues());
 
-			OutputStream imageFileOS;
-			try {
-				imageFileOS = getContentResolver().openOutputStream(uriTarget);
-				imageFileOS.write(arg0);
-				imageFileOS.flush();
-				imageFileOS.close();
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}  */
-			
-			camera.startPreview();
-//			camera.startFaceDetection();
+            InputStream is = new ByteArrayInputStream(arg0);
+            Bitmap bmp = BitmapFactory.decodeStream(is);
+
+            if (bmp.getWidth() % 2 == 1)
+                bmp = Bitmap.createBitmap(bmp, 0, 0, bmp.getWidth()-1, bmp.getHeight());
+
+            FaceDetector.Face[] faces;
+            FaceDetector face_detector = new FaceDetector(bmp.getWidth(), bmp.getHeight(), 5);
+            faces = new FaceDetector.Face[5];
+            int face_count = face_detector.findFaces(bmp, faces);
+
+            System.out.println(face_count);
+            for (int i = 0; i < face_count; i++) {
+                bmp = bmp.copy(Bitmap.Config.ARGB_8888, true);
+                Canvas canvas = new Canvas(bmp);
+                Paint paint = new Paint();
+                paint.setColor(Color.RED);
+                canvas.drawBitmap(bmp, new Matrix(), null);
+                PointF midpoint = new PointF();
+                faces[i].getMidPoint(midpoint);
+                canvas.drawRect(midpoint.x - faces[i].eyesDistance(), midpoint.y - faces[i].eyesDistance(),
+                        midpoint.x + faces[i].eyesDistance(), midpoint.y + faces[i].eyesDistance(), paint);
+            }
+
+            iv.setImageBitmap(bmp);
 		}
 	};
 
